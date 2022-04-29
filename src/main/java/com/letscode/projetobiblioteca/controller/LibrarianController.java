9bee1cd2f047902c03b8ba3c67115c331f984117
HannahFreitas <hannahcassia@gmail.com>
2022-04-29 17:53:03 -0300

    add validations

--- src/main/java/com/letscode/projetobiblioteca/controller/LibrarianController.java
+++ src/main/java/com/letscode/projetobiblioteca/controller/LibrarianController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -27,7 +28,7 @@ public class LibrarianController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Librarian save(@RequestBody Librarian librarian) {
+    public Librarian save(@RequestBody @Valid Librarian librarian) {
         return this.librarianService.save(librarian);
     }
 
--- src/main/java/com/letscode/projetobiblioteca/model/Librarian.java
+++ src/main/java/com/letscode/projetobiblioteca/model/Librarian.java
@@ -3,6 +3,7 @@ package com.letscode.projetobiblioteca.model;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @NoArgsConstructor
@@ -17,17 +18,22 @@ public class Librarian {
     private long id;
 
     @Column(name = "name")
+    @NotBlank(message = "Write your name.")
     private String name;
 
     @Column(name = "email")
+    @NotBlank(message = "Write your email.")
     private String email;
 
     @Column(name = "cpf")
+    @NotBlank(message = "Write your cpf.")
     private String cpf;
 
     @Column(name = "phone")
+    @NotBlank(message = "Write your phone number.")
     private String phone;
 
     @Column(name = "password")
+    @NotBlank(message = "Write your password.")
     private String password;
 }
