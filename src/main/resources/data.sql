0d3d5df80f360d42326f7d38d02892b0a62fbaa7
HannahFreitas <hannahcassia@gmail.com>
2022-05-02 11:34:50 -0300

    Add schema and data sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into librarian values('Paula', '11111111111', 'paula@gmail.com', '11111111111', 'teste');
+insert into book values('thewitcher','andre', 2021, 'teste', 5);
+insert into student values('Rhuan', '11111111111', 'alu232312313');
+insert into loan values(1, 1, '2022-05-02', null, '2022-05-17');
